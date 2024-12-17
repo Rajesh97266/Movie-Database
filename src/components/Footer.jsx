04ad2a1952701233abd7131425d4de21b6be4e5a
Rajesh97266 <rajesh97266@gmail.com>
2024-12-17 06:54:32 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="container">
+      <footer className="py-3 my-3 border-top">
+      <p className="text-center text-body-secondary">Copy right &copy; 2025 MovieHunt , Inc</p>
+      </footer>
+    </div>
   )
 }
 
